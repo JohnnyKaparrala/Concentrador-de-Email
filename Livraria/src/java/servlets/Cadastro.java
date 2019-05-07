4bd7d988bc85b25dcada92020a21b2eb194a51d7
Guilherme Rojas <guiribrojas@gmail.com>
2019-05-07 07:41:38 -0300

    projeto de servlet certo. . .

---
+++ Livraria/src/java/servlets/Cadastro.java
@@ -0,0 +1,106 @@
+package servlets;
+
+import bd.daos.Livros;
+import bd.dbos.Livro;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 17182 e 17186
+ */
+@WebServlet(name = "Cadastro", urlPatterns = {"/Cadastro"})
+public class Cadastro extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            //head+title
+            out.println("<head><title>Servlet Cadastro</title></head>");            
+            out.println("<body>");
+            out.println("<h1>Servlet Cadastro at " + request.getContextPath() + "</h1><br>");
+            out.println("</body></html>");
+        
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        try
+        {
+
+            int cod = Integer.parseInt(request.getParameter("codigo"));
+
+            String nm = request.getParameter("nome");
+
+            float pr = Float.parseFloat(request.getParameter("preco"));
+
+            Livro liv = new Livro(cod, nm, pr);
+
+            Livros.incluir(liv);
+
+            response.sendRedirect("Foi.html");
+        }
+        catch (Exception erro)
+        {
+            response.sendRedirect("Falho.html?msg="+erro.getMessage());
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
